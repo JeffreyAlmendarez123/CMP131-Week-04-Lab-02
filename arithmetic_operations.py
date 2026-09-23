9dd936967b0d0e3be8b97efe9164bbcef78d118f
almendarez.jeffre <almendarez.jeffre@student.ccm.edu>
2026-09-23 08:36:32 -0400

    complete Week 4 lab 1

---
+++ arithmetic_operations.py
@@ -0,0 +1,16 @@
+#Jeffrey Almendarez
+#CMP131
+#Week4
+#Lab02
+#Arithmetic_operations
+#9/23/26
+
+num1=float(input("Enter the first number:")) #We are prompting the user for two numbers and it will convert it as a whole number or decimal.
+num2=float(input("Enter the second number"))
+addition=num1+num2 #Both of variables displaying the addition
+subtraction=num1-num2 #The variables will display the subtraction
+multiplication=num1*num2 #Will show the multiplcation between them
+division=num1/num2 #The division between the two varibables
+power=num1**num2 #Will show us the variables exponent 
+average=(num1+num2)/2 #Display the average for us between the two variables
+
